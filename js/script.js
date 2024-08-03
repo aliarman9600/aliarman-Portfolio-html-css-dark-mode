92c85a9a01431d3dcd3bfa93e2c43587649acdc1
alrman_ali <aliarman9600@gmail.com>
2024-08-03 12:37:25 +0530

    Change index.html filename

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* ==================== typing animation ======================= */
+
+var typed = new Typed('.typing', {
+    strings: ['Frontend Developer', 'YouTuber', 'Web Developer', "Web Designer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    bacDelay: 1000,
+    loop: true
+});
